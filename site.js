f8f92a48ada1323543fb633e3b4263c78d96c07d
rash385 <sivkostja@gmail.com>
2018-03-16 12:04:44 +0300

    Update site.js

--- site.js
+++ site.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 app.get('/test', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res("test-bot");
+    res.send('test-bot');
 });
 
 app.listen(port, function() {

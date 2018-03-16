147e2b614ad662a53f6ddfdb5cab96918b30b4fe
rash385 <sivkostja@gmail.com>
2018-03-16 13:02:23 +0300

    Update site.js

--- site.js
+++ site.js
@@ -20,8 +20,29 @@ app.get('/', function(req, res) {
 
 app.get('/test', function(req, res) {
 
-    // ejs render automatically looks in the views folder
-    res.send(req.params.hub.challenge);
+    // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "francis"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
 
 app.listen(port, function() {

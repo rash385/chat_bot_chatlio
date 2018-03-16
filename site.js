0d7449067ae88e2aabeece9dec56ea6048ae4e4c
rash385 <sivkostja@gmail.com>
2018-03-16 11:30:01 +0300

    Update site.js

--- site.js
+++ site.js
@@ -15,7 +15,8 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('chat');
+    //res.render('chat');
+    res="chat-bot"
 });
 
 app.listen(port, function() {

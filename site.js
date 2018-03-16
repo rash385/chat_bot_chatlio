47f2cd439e886410bfd9711b686495573165fc07
rash385 <sivkostja@gmail.com>
2018-03-16 12:26:46 +0300

    Update site.js

--- site.js
+++ site.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 app.get('/test', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.send(request.params.hub_challenge);
+    res.send(request.params);
 });
 
 app.listen(port, function() {
